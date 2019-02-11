8be09451e85d33aef909d953c148bf48bf6aa070
noncense <noncensexnoncense@gmail.com>
2019-02-11 22:15:00 +0900

    #1 PregUtility

--- src/presto/utilities/PregUtility.php
+++ src/presto/utilities/PregUtility.php
@@ -1,8 +1,12 @@
 <?php
 namespace Presto\Utilities;
 
+use Presto\Traits\Singletonable;
+
 class PregUtility
 {
+    use Singletonable;
+
     public static function all(string $pattern, string $text)
     {
         preg_match_all($pattern, $text, $rows);
